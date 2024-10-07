e3849aaf9389489092fc6f723380c1a8c190eb59
unknown <naninarendra2000@gmail.com>
2024-10-07 12:00:30 -0700

    deploy

---
+++ src/utilis/firebase.js
@@ -0,0 +1,17 @@
+/ Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmkLe7I7Sqa-kpXv2r8vQpsdl4Cnz2KRw",
+  authDomain: "portfoliokadiveti.firebaseapp.com",
+  projectId: "portfoliokadiveti",
+  storageBucket: "portfoliokadiveti.appspot.com",
+  messagingSenderId: "549669167131",
+  appId: "1:549669167131:web:0683033a9d5efe9c9798be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
